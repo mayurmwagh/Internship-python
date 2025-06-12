218e884ac2328b87561a1c9d1b77e16e3e1b2372
mayurwagh <mayurwagh1412@gmail.com>
2025-06-12 14:08:05 +0530

    Data_types added

---
+++ data_types.py
@@ -0,0 +1,37 @@
+
+#LIST
+
+a = [1,2,3,55,3 ] 
+print(a)
+
+a[2] = "Mayur"
+print(a)
+
+a[4] = "Virat"
+print(a)
+
+a.remove('Mayur')
+print(a)
+
+
+a.remove('Virat')
+print(a)
+
+a.sort() 
+print(a)
+
+# b = 89,70,54
+# a.append(b)
+print(a)
+
+a[2] =90
+print(a)  
+
+a.sort() 
+print(a)
+
+a[1] = 100
+print(a)
+
+a.sort()
+print(a)
---
+++ sample.py
@@ -0,0 +1,92 @@
+
+
+#Sequential Data Type 
+# List , Tuple, Directory, Sets, 
+# List
+# Definition: A list is an ordered, mutable (changeable) collection.
+
+# Syntax: my_list = [1, 2, 3, 'apple']
+
+# Key Features:
+
+# Supports different data types
+
+# Allows indexing and slicing
+
+# Supports methods like append(), remove(), sort(), etc.
+
+
+
+# 2. Tuple
+# Definition: A tuple is an ordered, immutable (unchangeable) collection.
+
+# Syntax: my_tuple = (1, 2, 3)
+
+# Key Features:
+
+# Faster than lists
+
+# Useful for fixed data
+
+# Supports indexing and slicing
+
+
+#  3. String
+# Definition: A string is a sequence of characters.
+
+# Syntax: my_string = "Hello"
+
+# Key Features:
+
+# Immutable
+
+# Supports slicing and string methods like upper(), lower(), split(), etc.
+
+
+a = [8,3,2,3,2]
+print(a)
+
+a[2] = "Mayur"
+print (a)
+
+a.remove("Mayur")
+print(a)
+
+a.sort()
+print(a)
+
+# Combine 2 list
+
+a = [1,2,6,3,7,2,3]
+b = [9,3,42,8,2]
+
+c = (a + b) 
+d = [(a), (b)]
+
+print(c)
+print(d)
+
+v = sum(a)
+print(v)
+
+#Tuple
+#Tuple can define in ()
+a = (2,34,1,4,7)
+
+print(a)
+
+#Dictionary: is used to store data in key value format
+b= {"a": "sahin", "b":"sehawag", "c":"Dravid"}
+
+print(b)
+
+x= b["a"]
+print(x)
+
+#To add in Dictionary 
+b["d"] = "Zaheer"
+print(b)
+
+#To delete in Dictionary 
+del b["b"]
+print(b)
